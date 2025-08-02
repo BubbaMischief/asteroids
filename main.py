9468b4f6643f7c3f0c841363b63b15897994862b
BubbaMischief <bubbamischief@gmail.com>
2025-08-02 13:04:38 -0700

    3: Update main, asteroid, asteroidfeild: black screen, moving triangle, moving circles

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids,updatable,drawable)
+    Player.containers = (updatable,drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -14,8 +23,9 @@ def main():
             if event.type == pygame.QUIT:
                 is_running = False
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for each_thing in drawable:
+            each_thing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) /1000
 
