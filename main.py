6106972aeab41c3ad5aa0affb1ee09726b91584b
BubbaMischief <bubbamischief@gmail.com>
2025-08-02 11:59:27 -0700

    2: Update all files, current: black screen with rotating triangle

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     is_running = True
     while(is_running):
@@ -10,7 +14,12 @@ def main():
             if event.type == pygame.QUIT:
                 is_running = False
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) /1000
+
+        
     pygame.quit()
 
 
